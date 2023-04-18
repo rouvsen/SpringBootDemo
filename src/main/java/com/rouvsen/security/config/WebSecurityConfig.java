47a1ae971b08a26517d7a378658622da7982b1a6
rovshan <rouvsen@gmail.com>
2023-04-18 13:36:52 +0400

    WebSecurity Configuration added

---
+++ src/main/java/com/rouvsen/security/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.rouvsen.security.config;
+
+
+import com.rouvsen.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(enforceUniqueMethods = false)
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig implements WebSecurityConfigurer {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v*/registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+        return http.build();
+    }
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+
+    }
+
+    @Override
+    public void init(SecurityBuilder builder) throws Exception {
+
+    }
+}
